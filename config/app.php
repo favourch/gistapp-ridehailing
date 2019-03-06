957fef813a112d9776c4b497ad7e8a53463c1741
SenseiFavour <fchukwuedo@gmail.com>
2019-03-06 16:43:07 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://gistapp-ridehailing.herokuapp.com/'),
+    'url' => env('APP_URL', 'http://gistapp-ridehailing.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
