7fbea6b9245a821676774da75026935c5be5dc93
SenseiFavour <fchukwuedo@gmail.com>
2019-03-07 09:22:01 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,27 +6,10 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-       
+ public function boot()
+ {
     \URL::forceRootUrl(\Config::get('app.url'));    
     if (str_contains(\Config::get('app.url'), 'https://')) {
         \URL::forceScheme('https');
     }
-   
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-}
+ }
